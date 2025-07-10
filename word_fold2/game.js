35d6c4f9469f43c0c37359d3139b1091a7c092e7
Westfall <nkwestfall@wpi.edu>
2025-07-10 16:10:04 -0400

    first commit

---
+++ word_fold2/game.js
@@ -0,0 +1,126 @@
+const boards = [
+    {
+        cells: [
+            ["E", "L", "W", "Y", "C"],
+            ["Y", "L", "O", "A", "N"],
+            ["U", "B", "L", "E", "E"],
+            ["E", "L", "P", "M", "V"],
+            ["P", "U", "R", "A", "U"]],
+        words: ["CYAN", "YELLOW", "PURPLE", "MAUVE", "BLUE"]
+    },
+    {
+        cells: [
+            ["E", "K", "O", "A", "P"],
+            ["A", "W", "L", "I", "R"],
+            ["N", "S", "F", "A", "T"],
+            ["L", "E", "E", "R", "A"],
+            ["A", "G", "G", "U", "J"]],
+        words: ["TAPIR", "EAGLE", "JAGUAR", "SNAKE", "WOLF"]
+    },
+    {
+        cells: [
+            ["H", "C", "N", "A", "N"],
+            ["Y", "R", "A", "A", "A"],
+            ["R", "E", "A", "Y", "B"],
+            ["F", "P", "P", "E", "R"],
+            ["I", "G", "A", "P", "A"]],
+        words: ["CHERRY", "PAPAYA", "BANANA", "PEAR", "FIG"]
+    },
+]
+
+function make_cell_list() {
+    let cells = [...document.getElementById("cell-holder").children];
+    let cell_board = [];
+    for (let i = 0; i < 25; i += 5) {
+        cell_board.push(cells.slice(i, i + 5))
+    }
+    return cell_board;
+}
+
+function setup_game(starting_cells) {
+    for (let x = 0; x < 5; x++) {
+        for (let y = 0; y < 5; y++) {
+            CELLS[y][x].firstElementChild.innerHTML = starting_cells[y][x];
+        }
+    }
+}
+
+const CELLS = make_cell_list();
+let selected_x = -1;
+let selected_y = -1;
+
+setup_game(boards[0].cells)
+document.getElementById("words").innerHTML = "Words to spell: " + boards[0].words.join(", ")
+
+
+function move(x, y) {
+    CELLS[selected_y][selected_x].classList.add("empty");
+    CELLS[y][x].firstElementChild.innerHTML = CELLS[selected_y][selected_x].firstElementChild.innerHTML + CELLS[y][x].firstElementChild.innerHTML;
+    CELLS[selected_y][selected_x].firstElementChild.innerHTML = ""
+    select(x, y);
+}
+
+function unselect(x, y) {
+    CELLS[y][x].classList.remove("selected");
+    selected_x = -1;
+    selected_y = -1;
+}
+
+function select(x, y) {
+    if (CELLS[y][x].firstElementChild.innerHTML.length > 0) {
+        if (selected_x >= 0 && selected_y >= 0)
+            CELLS[selected_y][selected_x].classList.remove("selected");
+        CELLS[y][x].classList.add("selected");
+        selected_y = y;
+        selected_x = x;
+    }
+}
+
+function is_close(a, b) {
+    return Math.abs(a - b) <= 1
+}
+
+function can_move(x, y) {
+    let can_move = is_close(selected_x, x) && selected_y == y || is_close(selected_y, y) && selected_x == x;
+
+    return selected_x >= 0 && selected_y >= 0 && can_move && CELLS[y][x].firstElementChild.innerHTML.length > 0
+}
+
+function on_click(x, y) {
+    if (selected_x == x && selected_y == y) {
+        unselect(x, y)
+    }
+    else if (can_move(x, y)) {
+        move(x, y)
+    } else {
+        select(x, y)
+    }
+}
+
+let accel = .001;
+let velocity = 2;
+let angle = 0;
+
+function on_frame() {
+    angle += velocity;
+    // velocity += accel;
+    // accel += .0001;
+    document.body.style = "--angle:" + angle + "deg;"
+    requestAnimationFrame(on_frame);
+}
+on_frame()
+
+// function slow_down() {
+//     velocity = Math.max(.5, velocity - .5)
+//     accel = .001;
+
+function start_game() {
+    document.getElementById("start").classList.add("hidden")
+    document.getElementById("game").classList.remove("hidden");
+    document.querySelectorAll(".rr").forEach(e => {
+        let duration = e.duration;
+        let seconds = Math.random()*duration;
+        e.currentTime = seconds;
+        e.play()
+    })
+}
